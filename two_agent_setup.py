190fd0bad92dd94682cdd03061c978d7909069fd
Finn Buck <finneganlbuck@gmail.com>
2026-09-24 23:04:22 +0200

    Got program working on gpu.

--- two_agent_setup.py
+++ two_agent_setup.py
@@ -1,24 +1,31 @@
 import os
+import torch
 os.environ["HF_HUB_OFFLINE"] = "1"
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
 from threading import Thread
 
 gen_model_name = "Qwen/Qwen2.5-3B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(gen_model_name)
-gen_model = AutoModelForCausalLM.from_pretrained(gen_model_name)
+gen_model = AutoModelForCausalLM.from_pretrained(gen_model_name, torch_dtype=torch.float16).to("cuda")
 
 starting_context = "You are a poet.\nIf the user's message is exactly the single word POEM, respond with exactly one original poem of exactly 12 lines and nothing else: no title, no preamble, no explanation, not quotation marks.\nIf the user's message is the word REVIEW followed by a poem, you are to respond with 1 sentence containing a rating out of 10 for the poem and your reason for the rating.\n"
 
 agent_contexts = [starting_context, starting_context]
 
+agents = []
+
+for num in range(2):
+    agents.append({"context": starting_context, "poem_list": []})
+
 for i in range(1):
 
-    for agent_num in range(len(agent_contexts)):
-        agent_contexts[agent_num] += f"\nOn day {i + 1} you wrote this: \n"
+    for agent in agents:
+
+        agent["context"] += f"\nOn day {i + 1} you wrote this: \n"
 
         messages = [
-            {"role": "system", "content": agent_contexts[agent_num]},
-            {"role": "user", "content":     "POEM"},
+            {"role": "system", "content": agent["context"]},
+            {"role": "user", "content": "POEM"}
         ]
 
         inputs = tokenizer.apply_chat_template(
@@ -32,19 +39,35 @@ for i in range(1):
         new_poem = ""
 
         for token in streamer:
-#            print(token, end="", flush=True)
+#           print(token, end="", flush=True)
             new_poem += token
 
-        agent_contexts[agent_num] += "\nYOUR POEM:\n" + new_poem + "\n"
+        agent["context"] += "\nYOUR POEM:\n" + new_poem + "\n"
+        agent["poem_list"].append(new_poem)
 
-    for agent_num in range(len(agent_contexts)):
+    for j in range(2):
 
-        agent_contexts[agent_num] += f"\nThat same day a fellow poet wrote this: \n"
+        messages = [
+            {"role": "system", "content": agents[j]["context"]},
+            {"role": "user", "content": "REVIEW\n" + agents[(j + 1) % 2]["poem_list"][i]}
+        ]
 
+        inputs = tokenizer.apply_chat_template(
+            messages, add_generation_prompt=True, return_tensors="pt", return_dict=True)
 
-for context in agent_contexts:
+        streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+        thread = Thread(target=gen_model.generate, kwargs=dict(**inputs, max_new_tokens=200, streamer=streamer))
+        thread.start()
+        
+        new_review = ""
+
+        for token in streamer:
+#           print(token, end="", flush=True)
+            new_review += token
+
+        agents[(j + 1) % 2]["context"] += "\nA fellow poet reviewed your poem as follows: \n" + new_review + "\n"
+
+for agent in agents:
     print("-"*70)
-    print(context)
-
-
-
+    print(agent["context"])
